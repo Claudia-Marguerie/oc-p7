717515272f86014d861ab9e2d06e19fd04b0b6df
Claudia Marguerie <c_guevara02@yahoo.fr>
2020-08-18 19:01:53 +0900

    feat(): Session du 18/08

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,15 +1,28 @@
 'use strict';
 
-const { sequelize } = require(".");
+const {
+  Model
+} = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    lastName: DataTypes.STRING,
-    firstName: DataTypes.STRING,
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    lastname: DataTypes.STRING,
+    firstname: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    userAdmin: DataTypes.BOOLEAN
+    password: DataTypes.STRING
+   }, {
+     sequelize,
+     modelName: 'User',
   });
-  sequelize.sync()
   return User;
-}
+};
--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -10,12 +10,13 @@ class userObject {
 const user = new userObject()
 
 function formValid() { 
-    document.getElementById('form').addEventListener('submit', signUp);
+    
 }
 
 const userId = '';
 
 function signUp(event){
+    event.preventDefault();
     const user = {};
     user.lastname = event.target.lastname.value; // on cree le tableau du user avec les donnés entrées dans le formulaire
     user.firstname = event.target.firstname.value;
@@ -23,16 +24,16 @@ function signUp(event){
     user.password = event.target.password.value;
 
     const userObject = JSON.stringify(user);
-    
-    axios.post('http://localhost:3001/signup', userObject).then((response) => {
-        userId = response.body//.userId
-        console.log(userId);
-        const { token } = response.body.token;
-        localStorage.setItem("userId", JSON.stringify(userId)) // on converti la liste en string pour qu'elle soit lisible par javascript. On écrasse le panier stocké en local avec le panier à 0
-        localStorage.setItem('token', token);
+    console.log(userObject)
 
-        document.location.assign('index.html');
+    axios.post('http://localhost:3000/api/users/signup', user).then((response) => {
+        const data = response.data
+        localStorage.setItem("user", JSON.stringify(data.user)) // on converti la liste en string pour qu'elle soit lisible par javascript. On écrasse le panier stocké en local avec le panier à 0
+        localStorage.setItem('token', data.token);
+        document.location.href = 'index.html'; // Redirection vers la page d'accueil
     }), (err) => {
             console.log(err)
         }
 }
+
+document.getElementById('form').addEventListener('submit', signUp);
